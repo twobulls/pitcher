6f472ff48e950d123bcb79c2e6655e0777ae0fe0
Ben Kersten <ben.kersten@twobulls.com>
2020-06-18 11:52:12 +1000

    Workflow/release actions (#1)
    * Adds action for release of package to pypi

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Pitcher
 
-Pitcher aims to be a python web framework for use with Amazon API Gateway and supports both the HTTP and REST API types.
+![build](https://github.com/twobulls/pitcher/workflows/build/badge.svg)
+
+Pitcher aims to be a python web framework for use with Amazon API Gateway with support for both the HTTP and REST API types.
 
 It is inspired by [Django REST framework](https://www.django-rest-framework.org/) and [Lambda API](https://github.com/jeremydaly/lambda-api).
 
--- setup.py
+++ setup.py
@@ -4,13 +4,27 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = os.getenv("VERSION", "0.1")
+__version__ = os.getenv("VERSION", "0.0.1")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="pitcher",
+    author="Ben Kersten",
     author_email="ben.kersten@twobulls.com",
     version=__version__,
     license="MIT",
-    description="Microframework",
+    url="https://github.com/twobulls/pitcher",
+    description="Python web framework for API Gateway",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests"]),
+    python_requires='>=3.7',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
